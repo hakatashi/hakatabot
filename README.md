16bc736f9de04bde57f7304a5184efc2745c2370
hakatashi <hakatasiloving@gmail.com>
2014-07-30 13:32:16 +0900

    Add data and README.md

---
+++ README.md
@@ -0,0 +1,19 @@
+ipadic-bot
+==========
+
+Working on [@ipadic](https://twitter.com/ipadic).
+
+# LICENSE
+
+## IPAdic
+
+Copyright 2000, 2001, 2002, 2003 Nara Institute of Science
+and Technology.  All Rights Reserved.
+
+See `COPYING.ipadic`.
+
+## Code
+
+Produced by [hakatashi](https://github.com/hakatashi).
+
+[MIT License](http://opensource.org/licenses/MIT). See `LICENSE`.
--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 var querystring = require('querystring');
 var request = require('request');
+var CronJob = require('cron').CronJob;
+var sqlite3 = require('sqlite3').verbose();
+var time = require('time');
 var config = require('./config');
+
 var APIBase = 'https://api.twitter.com/1.1/';
+var db = new sqlite3.Database('ipadic.sqlite3');
 
 var twitter = function (method, resource, params, callback) {
+	var paramString = querystring.stringify(params)
+		.replace(/\!/g, "%21")
+		.replace(/\'/g, "%27")
+		.replace(/\(/g, "%28")
+		.replace(/\)/g, "%29")
+		.replace(/\*/g, "%2A"); // f*cking twitter implementation
+
 	request({
-		url: APIBase + resource + '.json' + '?' + querystring.stringify(params),
-		oauth: {
-			consumer_key: config.ACCESS.CONSUMER.KEY,
-			consumer_secret: config.ACCESS.CONSUMER.SECRET,
-			token: config.ACCESS.TOKEN.KEY,
-			token_secret: config.ACCESS.TOKEN.SECRET,
-		},
+		url: APIBase + resource + '.json' + '?' + paramString,
+		oauth: config.oauth,
 		json: true,
 		method: method,
 	}, callback);
@@ -25,9 +32,17 @@ twitter.post = function (resource, params, callback) {
 	twitter('POST', resource, params, callback);
 };
 
-twitter.get('users/show', {
-	screen_name: 'hakatashi'
-}, function (error, responce, data) {
-	if (error) console.error(error);
-	console.log(data);
-});
+var cronJob = new CronJob('00 */10 * * * *', function () {
+	db.get('SELECT * FROM ipadic ORDER BY RANDOM()', function (error, entry) {
+		twitter.post('statuses/update', {
+			status: entry.entry
+		}, function (error, responce, data) {
+			if (error) {
+				console.error(error);
+				return;
+			}
+
+			console.log(data);
+		});
+	});
+}, null, true, 'Asia/Tokyo');
