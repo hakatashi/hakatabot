462813beeb3b262f1f511b0fdbbfb43d3b35ded5
Koki Takahashi <hakatasiloving@gmail.com>
2014-12-08 22:35:12 +0900

    Add support for multiple accounts

--- hideoalert/index.js
+++ hideoalert/index.js
@@ -1,12 +1,12 @@
 var CronJob = require('cron').CronJob;
 var time = require('time');
+var async = require('async');
 var fs = require('fs');
 
 var twitter = require('../twitter');
 
-var alertTimes = 0;
-
 var frequency = 5;
+var targets = ['hideo54', 'akouryy1'];
 
 function repeat(string, times) {
 	var ret = '';
@@ -14,39 +14,52 @@ function repeat(string, times) {
 	return ret;
 }
 
-module.exports = new CronJob('00 * * * * *', function () {
+module.exports = new CronJob('30 * * * * *', function () {
 	var now = new Date();
 
-	fs.readFile(__dirname + '/lastalert.txt', function (error, data) {
-		if (error) return console.error(error);
+	async.waterfall([
+		function (done) {
+			fs.readFile(__dirname + '/progress.json', done);
+		},
+		function (data, done) {
+			var progresses = JSON.parse(data);
 
-		var lastAlert = data.toString();
+			async.each(targets, function (target, done) {
+				var progress = progresses[target];
 
-		twitter.get('hakatashi', 'statuses/user_timeline', {
-			screen_name: 'hideo54',
-			since_id: lastAlert,
-			count: frequency,
-			include_rts: false
-		}, function (error, responce, data) {
-			if (error) return console.error(error);
-			if (data.length < frequency) return;
+				twitter.get('hakatashi', 'statuses/user_timeline', {
+					screen_name: target,
+					since_id: progress.lastAlert,
+					count: frequency,
+					include_rts: false
+				}, function (error, responce, data) {
+					if (error) return done(error);
+					if (data.length < frequency) return done();
 
-			var lastTweet = data[0];
-			var borderTweet = data[frequency - 1];
-			var borderTime = new Date(borderTweet.created_at);
-			var lastTime = new Date(lastTweet.created_at);
+					var lastTweet = data[0];
+					var borderTweet = data[frequency - 1];
+					var borderTime = new Date(borderTweet.created_at);
+					var lastTime = new Date(lastTweet.created_at);
 
-			if (lastTime - borderTime < 10 * 60 * 1000) {
-				twitter.post('hakatashi', 'statuses/update', {
-					status: '@hideo54 勉強しろ' + repeat('!', alertTimes),
-					in_reply_to_status_id: lastTweet.id_str
+					if (lastTime - borderTime < 10 * 60 * 1000) {
+						twitter.post('hakatashi', 'statuses/update', {
+							status: '@' + target + ' 勉強しろ' + repeat('!', progress.alertTimes),
+							in_reply_to_status_id: lastTweet.id_str
+						});
+
+						progress.lastAlert = lastTweet.id_str;
+						progress.alertTimes++;
+					}
+
+					done();
 				});
+			}, function (error) {
+				if (error) return done(error);
 
-				lastAlert = lastTweet.id_str;
-				alertTimes++;
-			}
-
-			fs.writeFile(__dirname + '/lastalert.txt', lastAlert);
-		});
+				fs.writeFile(__dirname + '/progress.json', JSON.stringify(progresses), done);
+			});
+		}
+	], function (error) {
+		if (error) console.error(error);
 	});
 }, null, true, 'Asia/Tokyo');
